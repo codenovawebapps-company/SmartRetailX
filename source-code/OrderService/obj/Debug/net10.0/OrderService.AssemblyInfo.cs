0dd4d115f4150e7a349371cacf74a53874439962
Hasthika Rishan <hasthikaR2276@gmail.com>
2026-08-14 01:06:04 +0530

    Cloud Formation Template Updated

--- source-code/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
+++ source-code/OrderService/obj/Debug/net10.0/OrderService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6274cf1451012aeb45c3c034dd48709158de7da6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52801daade1094baa52f7f372992333b16fe9636")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- source-code/ProductService/Program.cs
+++ source-code/ProductService/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -23,5 +24,6 @@ app.MapGet("/", () => Results.Redirect("/swagger"));
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
